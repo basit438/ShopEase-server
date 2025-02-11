c540f1732d5113df160e627ff94da65979c3a0db
Basit <basitmanzoor438@gmail.com>
2025-02-11 21:17:27 +0530

    fixed corrss

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 const corsOptions = {
-    origin: 'https://e-commerce-fst-lpvt.vercel.app/',          
+    origin: '*',          
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
