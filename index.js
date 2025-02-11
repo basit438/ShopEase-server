50aeed60e1920e9c7bd531274c3dd656307cb0c4
Basit <basitmanzoor438@gmail.com>
2025-02-11 13:37:28 +0530

    fixed cors

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 const corsOptions = {
-    origin: process.env.NODE_ENV === "production"
-        ? "https://e-commerce-fst-pjc4.vercel.app"  // Your frontend URL
-        : "http://localhost:5057",                  // Local frontend for development
+    origin: 'https://e-commerce-fst-lpvt.vercel.app/',               // Local frontend for development
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
